5f6aa2f50521c687e5445dec1b4682338e4c30f5
MisterioSST <misteriotim77@gmail.com>
2018-04-10 17:08:59 +0300

    newcommit1

---
+++ Nof.rb
@@ -0,0 +1,10 @@
+puts "Hello, What your Name? " 
+my_name = gets.chomp
+
+puts "How old are you? "
+my_old = gets.to_i
+
+puts "What your Family Name? "
+my_family = gets.chomp
+
+print "my_name = gets.chomp" + " " + "my_old = gets.to_i" + " " + "my_family = gets.chomp"
